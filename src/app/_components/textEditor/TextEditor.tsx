f6b9d88f5b0b5a5d355d15135c92b2e55366fbbc
Mennatullah Ibraheem <mennaibraheem2@gmail.com>
2024-02-05 12:14:50 +0200

    dynamic importing

--- src/app/_components/textEditor/TextEditor.tsx
+++ src/app/_components/textEditor/TextEditor.tsx
@@ -1,18 +1,14 @@
 "use client";
 import "react-quill/dist/quill.snow.css";
 import React, { useState } from "react";
+
+const QuillEditor = dynamic(() => import("react-quill"), { ssr: false });
 import dynamic from "next/dynamic";
 
-import ReactQuill, { Quill } from "react-quill";
-
-// let SizeStyle = Quill.import("attributors/style/size");
-// SizeStyle.whitelist = ["10px", "15px", "18px", "20px", "32px", "54px"];
-// Quill.register(SizeStyle, true);
 const modules = {
   toolbar: [
     [{ header: "1" }, { header: "2" }, { font: [] }],
     [{ size: [] }],
-
     ["bold", "italic", "underline", "strike", "blockquote"],
     [
       {
@@ -57,20 +53,20 @@ const TextEditor = () => {
   return (
     <>
       <div className="h-full w-full flex items-center m-2 flex-col">
-        <ReactQuill
+        <QuillEditor
           formats={formats}
           modules={modules}
           className=" w-[800px] h-[500px]"
           value={value}
           theme="snow"
           onChange={setValue}
-        ></ReactQuill>
+        ></QuillEditor>
       </div>
-      {/* <div className="bg-gray-">
+      {/* <div className="">
         <div dangerouslySetInnerHTML={{ __html: value }}></div>
       </div> */}
     </>
   );
 };
 
-export default TextEditor;
+export default dynamic(() => Promise.resolve(TextEditor), { ssr: false });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
-
 import TextEditor from "./_components/textEditor/TextEditor";
 
+// eslint-disable-next-line import/no-unused-modules
 export default function Home() {
   return (
     <div className="h-screen w-full">
